4b5d934c22b65c809948f18b38046d53abe05253
ldcruzl <lcruzl@unjbg.edu.pe>
2024-07-13 15:57:00 -0500

    Actualizacion registros1: Se culmino con el programa, asegurando su funcionamiento

--- registros1.cpp
+++ registros1.cpp
@@ -2,18 +2,21 @@
 
 #include<iostream>
 #include<string>
+#include<conio.h>
 using namespace std;
 
-struct EMP{
+struct EMPLEADO{
     int sue;//variable para el sueldo
     string sex;//variable para el sexo
     string name;//variable para los nombres
 };
 
 int main(){
-    int n;
+    int n, msue=0, mensue=0;
+    system("cls");
+    cout<<"Empresa DataTech"<<endl;
     cout<<"Ingrese el numero de empleados: ";cin>>n;
-    EMP empleado[n];
+    EMPLEADO empleado[n];
     for(int i=0; i<n; i++){
         cout<<"Ingrese los datos del empleado #"<<i+1<<endl;
         cin.ignore();
@@ -21,5 +24,24 @@ int main(){
         cout<<"Ingrese su sexo: ";cin>>empleado[i].sex;
         cout<<"Ingrese su sueldo: ";cin>>empleado[i].sue;
     }
+    for(int i=0; i<n; i++){
+        if(empleado[i].sue<empleado[mensue].sue){
+            mensue=i;
+        }
+        if(empleado[i].sue>empleado[msue].sue){
+            msue=i;
+        }
+    }
+    system("cls");
+    cout<<"Empleado con el menor sueldo"<<endl;
+    cout<<"Nombre: "<<empleado[mensue].name<<endl;
+    cout<<"Sexo: "<<empleado[mensue].sex<<endl;
+    cout<<"Sueldo: "<<empleado[mensue].sue<<endl;
+    system("pause");
+    cout<<"Empleado con el mayor sueldo"<<endl;
+    cout<<"Nombre: "<<empleado[msue].name<<endl;
+    cout<<"Sexo: "<<empleado[msue].sex<<endl;
+    cout<<"Sueldo: "<<empleado[msue].sue<<endl;
+    system("pause");
     return 0;
 }
