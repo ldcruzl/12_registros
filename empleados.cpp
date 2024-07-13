b04e7aaaedc54e08c9e1a5b8ceb5bc0eafa3119c
ldcruzl <lcruzl@unjbg.edu.pe>
2024-07-13 10:54:46 -0500

    Actualizacion empleados: Se culmino hasta donde se avanzo en clase

--- empleados.cpp
+++ empleados.cpp
@@ -1,6 +1,7 @@
 //CRUZ LLICA, Luis David
 
 #include<iostream>
+#include<conio.h>
 using namespace std;
 
 struct EMP{
@@ -19,12 +20,18 @@ int main(){
         cin.ignore();
         cout<<"Ingrese el nombre del empleado: ";getline(cin, EMPLEADO[i].nombre);
         for(int j=0; j<12; j++){
-            cout<<"Ingrese la cantidad de ventas: ";cin>>EMPLEADO[i].ventas[j];
+            cout<<"Ingrese las ventas del mes: ";cin>>EMPLEADO[i].ventas[j];
         }
         cout<<"Ingrese su salario: ";cin>>EMPLEADO[i].salario;
     }
     for(int i=0; i<n; i++){
         cout<<"EMPLEADO #"<<i+1<<endl;
+        cout<<"El empleado es: "<<EMPLEADO[i].nombre<<endl;
+        cout<<"Su numero es: "<<EMPLEADO[i].numero<<endl;
+        for(int j=0; j<12; j++){
+            cout<<"Las ventas fueron: "<<EMPLEADO[i].ventas[j]<<endl;
+        }
+        cout<<"El salario es de: "<<EMPLEADO[i].salario<<endl;
     }
     return 0;
 }
