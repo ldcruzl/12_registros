a79c1fbe5eca9ba5c8bf6959476943eb22f11c0a
ldcruzl <lcruzl@unjbg.edu.pe>
2024-07-13 23:48:26 -0500

    Actualizacion registros3: Se culmino con la busqueda de un mes, mostrando los datos coincidientes

--- registros3.cpp
+++ registros3.cpp
@@ -23,5 +23,25 @@ int main(){
         cout<<"Ingrese numero del mes de nacimiento: ";cin>>persona[i].mes;
         cout<<"Ingrese el anio de nacimiento: ";cin>>persona[i].year;
     }
+    int mes;
+    cout<<"---------Personas que nacieron en el mismo mes---------"<<endl;
+    do{
+        cout<<"Ingrese un mes a buscar: ";cin>>mes;
+        if(mes!=0){
+            int c=0;
+            for(int i=0; i<n; i++){
+                if(persona[i].mes==mes){
+                    cout<<"Nombre: "<<persona[i].name<<endl;
+                    cout<<"Dia de nacimiento: "<<persona[i].dia<<endl;
+                    cout<<"Mes de nacimiento: "<<persona[i].mes<<endl;
+                    cout<<"Anio de nacimiento: "<<persona[i].year<<endl;
+                    c++;
+                }
+            }
+            if(c==0){
+                cout<<"No hay coincidencias, pruebe otro mes"<<endl;
+            }
+        }
+    }while(mes!=0);
     return 0;
 }
